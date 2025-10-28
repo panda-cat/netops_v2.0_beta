8da2bb4681d715c861184e46d71f01a5c4c4fdbf
panda-cat <90021060+panda-cat@users.noreply.github.com>
2025-10-28 14:52:02 +0800

    Update netops_start.py

--- netops_start.py
+++ netops_start.py
@@ -16,9 +16,13 @@ from lib import comm
 
 def StartedTheEngine():
     exists_xlsx = os.path.normpath(os.path.join(comm.BASE_PATH, 'inventory', 'devices.xlsx'))
-    os.system(f"python {comm.BASE_PATH}/func/func_list.py")
-    sys.exit()
-
+    if os.path.exists(exists_xlsx):
+        os.system(f"python {comm.BASE_PATH}/func/func_list.py")
+        sys.exit()
+    else:
+        print('-' * 42)
+        print(colored('请检查当前文件夹inventory中devices.xlsx文件', 'blue'))
+        sys.exit()
 
 # 命令行窗口标题
 system("title Python-NetOps_2.0_beta")
