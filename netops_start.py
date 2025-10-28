ee0acef240a70636faf35ffdc908c099591214a6
panda-cat <90021060+panda-cat@users.noreply.github.com>
2025-10-28 15:15:56 +0800

    Update netops_start.py

--- netops_start.py
+++ netops_start.py
@@ -13,12 +13,15 @@ import pandas as pd
 from termcolor import colored
 
 from lib import comm
+from func import func_list
 
 def StartedTheEngine():
     exists_xlsx = os.path.normpath(os.path.join(comm.BASE_PATH, 'inventory', 'devices.xlsx'))
     if os.path.exists(exists_xlsx):
-        os.system(f"python {comm.BASE_PATH}/func/func_list.py")
-        sys.exit()
+        # os.system(f"python {comm.BASE_PATH}/func/func_list.py") # <--- 2. 删除这一行
+        # sys.exit()                                              # <--- 3. 删除这一行
+        func_list.run()                                         # <--- 4. 添加这一行，直接调用
+        sys.exit()                                              # <--- 5. 在调用后退出
     else:
         print('-' * 42)
         print(colored('请检查当前文件夹inventory中devices.xlsx文件', 'blue'))
@@ -28,5 +31,4 @@ def StartedTheEngine():
 system("title Python-NetOps_2.0_beta")
 
 if __name__ == "__main__":
-
     StartedTheEngine()
